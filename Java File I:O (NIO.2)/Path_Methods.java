afb536d1f31a0230f8d80fdca4e45ecca4444037
Andrei Kudrin <andreikudrin@gmail.com>
2020-05-23 19:48:07 +0100

    Java File I/O (NIO.2) - Path methods - added more methods

--- Java File I:O (NIO.2)/Path_Methods.java
+++ Java File I:O (NIO.2)/Path_Methods.java
@@ -2,11 +2,16 @@ import java.nio.file.Path;
 
 public class Path_Methods {
     public static void main(String[] args) {
+
+        String HOME = System.getProperty("user.home");
+        System.out.println(HOME);
+
         Path path = Path.of("a/b/c");
         Path root = path.getRoot();
         Path parent = path.getParent();
         Path file = path.getFileName();
-        System.out.println(root + " " + parent + " " + file);
+        Path subpath = path.subpath(0, 2)
+        System.out.println(root + " " + parent + " " + file + " " + subpath);
 
     }
 }
